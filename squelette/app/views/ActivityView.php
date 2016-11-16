252410a21b383414b77afd169397a7bb010cb478
Ismail Daoudy <Ismail Daoudy>
2016-11-16 10:54:19 +0100

    edit ActivityView

--- squelette/app/views/ActivityView.php
+++ squelette/app/views/ActivityView.php
@@ -27,6 +27,41 @@ class EventView extends AbstractView
                 $this->register();
                 break;
         }
+        $framework = $this->app_root.'/css/vandenbr3u_library/css/theme.css';
+        $style_file = $this->app_root.'/css/css/style.css';
+        $header = $this->renderHeader();
+        $menu   = $this->renderMenu();
+        $footer = $this->renderFooter();
+
+
+        $html = <<<EOT
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>SportNet</title>
+        <link rel="stylesheet" href="${framework}">
+        <link rel="stylesheet" href="${style_file}">
+    </head>
+
+    <body class="grid_float">
+
+        <header class="row">${header}</header>
+
+        <div class="row menu">
+            ${menu}
+    	</div>
+        <section class="row content offset_1 column_6">
+            ${main}
+        </section>
+
+        <footer> ${footer} </footer>
+
+    </body>
+</html>
+EOT;
+
+    echo $html;
     }
 
     public function detail(){
