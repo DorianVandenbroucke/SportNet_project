245560f43dd5f92bc38fc74282486425e22ff0fc
Ismail Daoudy <Ismail Daoudy>
2016-11-17 15:46:30 +0100

    add ParticipantView pour affichier un récap des inscriptions

---
+++ squelette/app/views/ParticipantView.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace app\views;
+
+
+class ParticipantView extends AbstractView
+{
+
+    public function __construct($data)
+    {
+        parent::__construct($data);
+    }
+
+    public function render($selector)
+    {
+        switch ($selector) {
+            case 'recap':
+                $main = $this->recap();
+                break;    
+        }
+        $framework = $this->app_root.'/css/vandenbr3u_library/css/theme.css';
+        $style_file = $this->app_root.'/css/css/style.css';
+        $header = $this->renderHeader();
+        $menu   = $this->renderMenu();
+        $footer = $this->renderFooter();
+
+
+        $html = <<<EOT
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>SportNet</title>
+        <link rel="stylesheet" href="${framework}">
+        <link rel="stylesheet" href="${style_file}">
+    </head>
+
+    <body class="grid_float">
+
+        <header class="row">${header}</header>
+
+        <div class="row menu">
+            ${menu}
+    	</div>
+        <section class="row content offset_1 column_6">
+            ${main}
+        </section>
+
+        <footer> ${footer} </footer>
+
+    </body>
+</html>
+EOT;
+
+    echo $html;
+    }
+
+    public function recap(){
+        $html = '<section>';
+        foreach ($this->data as $inscription) {
+                     $html .= ' nom: '.$inscription->name.'<br>date: '.substr($inscription->date,0,10).'<br> heure :'.substr($inscription->date,10,6).'<br> price: '.$inscription->price.'<hr>';
+        }   
+        $html .='</section>';
+        return $html;
+    }
+
+}
