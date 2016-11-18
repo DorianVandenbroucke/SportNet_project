a04023a945dc9ae115b8b8064e82ddb9e798030f
marco <suyakuq@gmail.com>
2016-11-18 18:09:41 +0100

    Ajouté une validation pour les dates

--- squelette/app/controllers/EventController.php
+++ squelette/app/controllers/EventController.php
@@ -55,12 +55,13 @@ class EventController extends AbstractController
                 $event = empty($id) ? new Event() : Event::find($id);
                 $event->name = $this->request->post['name'];
                 $event->description = filter_var($this->request->post['description'], FILTER_SANITIZE_SPECIAL_CHARS);
+
+                if(!Util::isDateValid($this->request->post['startDate']) || !Util::isDateValid($this->request->post['endDate'])){
+                    $this->redirectTo($this->request->script_name."/event/add/".$redirectParam);
+                    return;
+                }
                 $event->startDate = Util::strToDate($this->request->post['startDate'], MYSQL_DATE_FORMAT);
                 $event->endDate = Util::strToDate($this->request->post['endDate'], MYSQL_DATE_FORMAT);
-                if(!Util::areDatesValid([$event->startDate,$event->endDate])){
-                    echo $this->request->script_name."/add/".$redirectParam;
-                    $this->redirectTo($this->request->script_name."/add/".$redirectParam);
-                }
                 $event->addresse = $this->request->post['addresse'];
                 $event->id_discipline = $this->request->post['id_discipline'];
 
@@ -71,7 +72,7 @@ class EventController extends AbstractController
                 if($event->save()) {
                     $this->redirectTo("../?id=$event->id");
                 }else{
-                    $this->redirectTo($this->request->script_name."/add/".$redirectParam);
+                    $this->redirectTo($this->request->script_name."/event/add/".$redirectParam);
                 }
             }
         }else{
