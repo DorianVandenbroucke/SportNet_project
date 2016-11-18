c19a5dac0efd446894c9c04a01b1988391af82da
DORIAN-PC\DorianHa <dorian.vandenbroucke@outlook.com>
2016-11-18 11:59:27 +0100

    class active menu

--- squelette/app/views/AbstractView.php
+++ squelette/app/views/AbstractView.php
@@ -68,10 +68,11 @@ abstract class AbstractView {
         $path_param = "";
         if (isset($_SERVER["PATH_INFO"])) {
             $path = $_SERVER["PATH_INFO"];
-            if($_SERVER["QUERY_STRING"]){
+            if($_SERVER["QUERY_STRING"] && $path == "/event/all/" && (isset($_SESSION['promoter']) && $_SERVER['QUERY_STRING'] == "id=".$_SESSION['promoter'])){
                 $path = $path."?".$_SERVER["QUERY_STRING"];
             }
         }
+
         $id_promoter = "";
         if (isset($_SESSION['promoter'])) {
             $id_promoter = $_SESSION['promoter'];
@@ -81,31 +82,37 @@ abstract class AbstractView {
 
         if (isset($_SESSION['promoter'])) {
             $array = array(
-              "" => "Accueil",
-              "/event/all/" => "Evénements",
-              "/event/add/" => "Ajouter un événement",
-              "/event/all/?id=".$_SESSION['promoter'] => "Mes événements",
-              "/logout/" => "Me déconnecter"
+              "Accueil" => array("", "/"),
+              "Evénements" => array("/event/all/", "/event/", "/activity/detail/", "/activity/register/", "/activity/participants/"),
+              "Ajouter un événement" => array("/event/add/"),
+              "Mes événements" => array("/event/all/?id=".$_SESSION['promoter']),
+              "Me déconnecter" => array("/logout")
             );
         }else if(!isset($_SESSION['promoter'])){
             $array = array(
-              "" => "Accueil",
-              "/event/all/" => "Evénements",
-              "/event/add/" => "Ajouter un événement",
-              "/signin/" => "Me connecter"
+              "Accueil" => array("", "/"),
+              "Evénements" => array("/event/all/", "/event/", "/activity/detail/", "/activity/register/", "/activity/participants/"),
+              "Ajouter un événement" => array("/event/add/"),
+              "Me connecter" => array("/signin/")
             );
         }
 
-        foreach($array as $lien => $nom){
-          if($lien === $path || $lien === "/"){
+        foreach($array as $nom => $liens){
+          $verif_link = 0;
+          $lien_o = $liens[0];
+          foreach($liens as $lien){
+            if($lien === $path){
+              $html .=
+                      "<li>
+                          <a href='$this->script_name".$lien_o."' class='active'>".$nom."</a>
+                      </li>";
+              $verif_link = 1;
+            }
+          }
+          if($verif_link == 0){
             $html .=
                     "<li>
-                        <a href='$this->script_name".$lien."' class='active'>".$nom."</a>
-                    </li>";
-          }else{
-            $html .=
-                    "<li>
-                        <a href='$this->script_name".$lien."'>".$nom."</a>
+                        <a href='$this->script_name".$lien_o."'>".$nom."</a>
                     </li>";
           }
         }
